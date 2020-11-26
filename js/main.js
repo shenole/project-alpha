21ed52af528531e406131396d40101c55ba876bd
Shenole Latimer <shenole@gmail.com>
2020-11-26 16:28:06 -0500

    cleaned out file

--- js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// MOBILE MENU FUNCTIONALITY
-const burgerNav = document.querySelector('#burger-nav');
-const menuModal = document.querySelector('#mobile-menu-modal');
-
-burgerNav.addEventListener('click', function () {
-    menuModal.classList.toggle('activate-mobile-menu');
-});
